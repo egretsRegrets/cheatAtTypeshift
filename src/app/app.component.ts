15ca9cce5e563441d05b6b3e5eaa56405f62a1f0
mercantilism <samuel.galen.gardner@gmail.com>
2017-04-27 18:01:59 -0700

    mergeSort, merge, compareWords functions to methods on AppComponent component class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,6 +17,9 @@ export class AppComponent implements OnInit {
 
   getRangeByLength;
   resolveWords;
+  mergeSort;
+  merge;
+  compareWords;
 
   addSlot = function(){
     this.wordListSeed.push(['a']);
@@ -52,51 +55,50 @@ export class AppComponent implements OnInit {
     };
 
     this.resolveWords = function(){
-      const {rangeStart, rangeEnd} = this.getRangeByLength(this.wordListSeed.length);
-      let narrowWordList: string[] = this.masterWordList.slice(rangeStart, rangeEnd);
-      this.resultWords = mergeSort(narrowWordList, this.wordListSeed);
+        const {rangeStart, rangeEnd} = this.getRangeByLength(this.wordListSeed.length);
+        let narrowWordList: string[] = this.masterWordList.slice(rangeStart, rangeEnd);
+        this.resultWords = this.mergeSort(narrowWordList);
 
-      console.log('solutions are', this.resultWords);
+        console.log('solutions are', this.resultWords);
+    }
+    this.mergeSort = function(array: string[]): string[] {
+        if (array.length <= 1 ){
+            return array;
+        }
+        const middle = Math.floor(array.length / 2);
+        const leftSide = array.slice(0, middle);
+        const rightSide = array.slice(middle);
+        return this.merge(this.mergeSort(leftSide), this.mergeSort(rightSide));
     }
 
-    function mergeSort(array: string[], userLetters: string[]): string[] {
-      if (array.length <= 1 ){
-          return array;
-      }
-      const middle = Math.floor(array.length / 2);
-      const leftSide = array.slice(0, middle);
-      const rightSide = array.slice(middle);
-      return merge(mergeSort(leftSide, userLetters), mergeSort(rightSide, userLetters), userLetters);
+    this.merge = function(leftSide: string[], rightSide: string[]): string[]{
+        let mergedArray: string[] = [];
+        const largerArrayLength = leftSide.length > rightSide.length ? leftSide.length : rightSide.length;
+        for (let i  = 0; i < largerArrayLength; i++) {
+            if(leftSide[i] && this.compareWords(leftSide[i])) {
+                mergedArray.push(leftSide[i]);
+            }
+            if(rightSide[i] && this.compareWords(rightSide[i])) {
+                mergedArray.push(rightSide[i]);
+            }
+        }
+        return mergedArray;
     }
 
-    function merge (leftSide: string[], rightSide: string[], userLetters): string[]{
-      let mergedArray: string[] = [];
-      const largerArrayLength = leftSide.length > rightSide.length ? leftSide.length : rightSide.length;
-      for (let i  = 0; i < largerArrayLength; i++) {
-          if(leftSide[i] && compareWords(leftSide[i], userLetters)) {
-              mergedArray.push(leftSide[i]);
-          }
-          if(rightSide[i] && compareWords(rightSide[i], userLetters)) {
-              mergedArray.push(rightSide[i]);
-          }
-      }
-      return mergedArray;
-    }
-
-    function compareWords (collectionWord, userLetters) {
-      if (collectionWord.length <= 0){
-          console.log("returning from compareWords with true"); // for tests
-          return true;
-      }
-      const userLetterSlot = userLetters[0];
-      for( let letterIndex = 0; letterIndex <= userLetterSlot.length; letterIndex++){
-          if (letterIndex === userLetterSlot.length){
-              return false;
-          }
-          if (collectionWord[0] === userLetterSlot[letterIndex]){  
-            return compareWords(collectionWord.slice(1), userLetters.slice(1));
-          }
-      }
+    this.compareWords = function(collectionWord, userLetters = this.wordListSeed) {
+        if (collectionWord.length <= 0){
+            console.log("returning from compareWords with true"); // for tests
+            return true;
+        }
+        const userLetterSlot = userLetters[0];
+        for( let letterIndex = 0; letterIndex <= userLetterSlot.length; letterIndex++){
+            if (letterIndex === userLetterSlot.length){
+                return false;
+            }
+            if (collectionWord[0] === userLetterSlot[letterIndex]){  
+            return this.compareWords(collectionWord.slice(1), userLetters.slice(1));
+            }
+        }
     }
     
   }
